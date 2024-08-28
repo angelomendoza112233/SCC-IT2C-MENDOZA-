c3c824fca1a267f00cf536b7c0a464a63d38b884
angelomendoza112233 <mendozaangeloa12@gmail.com>
2024-08-28 14:13:47 +0800

    product

--- src/javaapplication3/JavaApplication3.java
+++ src/javaapplication3/JavaApplication3.java
@@ -16,13 +16,15 @@ public class JavaApplication3 {
 //        netPay gpay=new netPay();
 //        gpay.getpay();
 //    
+            product p = new product();
+            p.addProduct(10011, "Soap", 20.00, 20, 30);
+            p.viewProduct();
         
-        
-     grade gn=new grade();
-     gn.addGrades(1011, "mike", 1.0, 1.0, 1.0, 1.0);
-     gn.viewGrade();
-         
-     
+//     grade gn=new grade();
+//     gn.addGrades(1011, "mike", 1.0, 1.0, 1.0, 1.0);
+//     gn.viewGrade();
+//         
+//     
      
         
         
